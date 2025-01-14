e095a12d0c7e686db93073bcdf37ebd49943c753
Alejandro <alejandro.cornejo23@gmail.com>
2025-01-14 17:13:08 +0100

    .

--- data_sets/data_set_neohookean_generator.py
+++ data_sets/data_set_neohookean_generator.py
@@ -64,16 +64,14 @@ def CalculateStressFromDeformationGradient(ConstitutiveLaw, Properties, Geometry
     dimension  = cl.WorkingSpaceDimension()
     voigt_size = cl.GetStrainSize()
 
-    # [geom, nnodes] = CreateGeometry(mdpa, dimension)
-
-    N = KM.Vector(nnodes)
-    DN_DX = KM.Matrix(nnodes, dimension)
+    N = KM.Vector()
+    DN_DX = KM.Matrix()
 
     cl.Check(Properties, Geometry, ModelPart.ProcessInfo)
 
     cl_options = KM.Flags()
     cl_options.Set(KM.ConstitutiveLaw.USE_ELEMENT_PROVIDED_STRAIN, False)
-    cl_options.Set(KM.ConstitutiveLaw.COMPUTE_CONSTITUTIVE_TENSOR, True)
+    cl_options.Set(KM.ConstitutiveLaw.COMPUTE_CONSTITUTIVE_TENSOR, False)
     cl_options.Set(KM.ConstitutiveLaw.COMPUTE_STRESS, True)
 
     # Define deformation gradient
@@ -85,11 +83,10 @@ def CalculateStressFromDeformationGradient(ConstitutiveLaw, Properties, Geometry
     constitutive_matrix = KM.Matrix(voigt_size,voigt_size)
 
     # Let's initialize the values
-    for i in range(0, voigt_size):
-        stress_vector[i] = 0.0
-        strain_vector[i] = 0.0
-        for j in range(0, voigt_size):
-            constitutive_matrix[i, j] = 0.0
+    stress_vector.fill(0.0)
+    strain_vector.fill(0.0)
+    constitutive_matrix.fill(0.0)
+    # s = np.array(stress_vector, copy=False)
 
     # Setting the parameters - note that a constitutive law may not need them all!
     cl_params = _set_cl_parameters(cl_options, F, detF, strain_vector, stress_vector, constitutive_matrix, N, DN_DX, ModelPart, Properties, Geometry)
@@ -114,11 +111,62 @@ def CalculateStressFromDeformationGradient(ConstitutiveLaw, Properties, Geometry
 #====================================================================================
 #====================================================================================
 
+def PredictStrainNormFromF(ConstitutiveLaw, Properties, Geometry, ModelPart, DeformationGradient):
+    # The selected CL
+    cl = ConstitutiveLaw
+
+    dimension  = cl.WorkingSpaceDimension()
+    voigt_size = cl.GetStrainSize()
+
+    N = KM.Vector()
+    DN_DX = KM.Matrix()
+
+    cl.Check(Properties, Geometry, ModelPart.ProcessInfo)
+
+    cl_options = KM.Flags()
+    cl_options.Set(KM.ConstitutiveLaw.USE_ELEMENT_PROVIDED_STRAIN, False)
+    cl_options.Set(KM.ConstitutiveLaw.COMPUTE_CONSTITUTIVE_TENSOR, False)
+    cl_options.Set(KM.ConstitutiveLaw.COMPUTE_STRESS, True)
+
+    # Define deformation gradient
+    F = KM.Matrix(dimension, dimension)
+    F[0,0] = DeformationGradient[0,0]
+    F[0,1] = DeformationGradient[0,1]
+    F[1,0] = DeformationGradient[1,0]
+    F[1,1] = DeformationGradient[1,1]
+    detF = 1.0
+
+    stress_vector = KM.Vector(voigt_size)
+    strain_vector = KM.Vector(voigt_size)
+    constitutive_matrix = KM.Matrix(voigt_size,voigt_size)
+
+    # Let's initialize the values
+    strain_vector.fill(0.0)
+
+    # Setting the parameters - note that a constitutive law may not need them all!
+    cl_params = _set_cl_parameters(cl_options, DeformationGradient, detF, strain_vector, stress_vector, constitutive_matrix, N, DN_DX, ModelPart, Properties, Geometry)
+
+    # Let's compute the strain and stress output
+    strain_calculate_value = KM.Vector(voigt_size)
+    strain_measure_variable = KM.GREEN_LAGRANGE_STRAIN_VECTOR # ALMANSI_STRAIN_VECTOR HENCKY_STRAIN_VECTOR GREEN_LAGRANGE_STRAIN_VECTOR
+
+    cl.CalculateValue(cl_params, strain_measure_variable, strain_calculate_value)
+
+    # return strain_vector, stress_vector
+    return np.linalg.norm(strain_calculate_value)
+
+#====================================================================================
+#====================================================================================
+
 def ResetF(F):
     F[0, 0] = 1.0
     F[0, 1] = 0.0
     F[1, 0] = 0.0
     F[1, 1] = 1.0
+
+#====================================================================================
+#====================================================================================
+
 
 '''
 Definition of the problem to solve
@@ -161,8 +209,10 @@ dimension = cl.WorkingSpaceDimension()
 voigt_size = cl.GetStrainSize()
 
 properties = KM.Properties(1)
-properties.SetValue(KM.YOUNG_MODULUS, 10e6)
-properties.SetValue(KM.POISSON_RATIO, 0.4)
+E = 1e7
+nu = 0.4
+properties.SetValue(KM.YOUNG_MODULUS, 1e7)
+properties.SetValue(KM.POISSON_RATIO, nu)
 properties.SetValue(KM.CONSTITUTIVE_LAW, cl)
 
 [geometry, nnodes] = CreateGeometry(model_part, dimension)
@@ -179,21 +229,43 @@ cl.InitializeMaterial(properties, geometry, KM.Vector(nnodes))
 
 # ----- Start the case loop
 angle_increment_deg = 10.0
-n_steps             = 100 # step in the loading history
-max_stretch_factor  = 0.01 # lambda
+n_steps             = 25 # step in the loading history
+max_stretch_factor  = 0.25 # lambda
 theta               = 0.0
 phi                 = 0.0
 # -----
+
+with open("neo_hookean_hyperelastic_law/data_set.log", "w") as file:
+    file.write("Dataset generated for a HyperElasticPlaneStrain2DLaw constitutive law \n \n")
+    file.write("Data used for the generation: \n")
+    file.write("\tYoung's Modulus = " + str(E) + "\n")
+    file.write("\tPoisson's ratio = " + str(nu) + "\n")
+    file.write("\tAngle increment = " + str(angle_increment_deg) + " deg \n")
+    file.write("\tNumber of steps per load history = " + str(n_steps) + "\n")
+
 strain_history = np.zeros((n_steps, voigt_size))
 stress_history = strain_history.copy()
-factor = max_stretch_factor / n_steps
 
 while theta <= 360.0 and phi <= 360.0:
+    dF11 = math.cos(math.radians(theta)) * max_stretch_factor
+    dF12 = 2.0 * math.sin(math.radians(theta)) * math.sin(math.radians(phi)) * max_stretch_factor
+    dF21 = dF12
+    dF22 = 2.0 * math.sin(math.radians(theta)) * math.cos(math.radians(phi)) * max_stretch_factor
+
+    # We check how much is the strain, hence we correct to maintain the norm
+    F_trial = KM.Matrix(dimension, dimension)
+    F_trial[0, 0] = 1.0 + dF11
+    F_trial[0, 1] = dF12
+    F_trial[1, 0] = F_trial[0, 1]
+    F_trial[1, 1] = 1.0 + dF22
+    predicted_strain_norm = PredictStrainNormFromF(cl, properties, geometry, model_part, F_trial)
+
     for step in range(n_steps):
-        F[0, 0] += math.cos(math.radians(theta)) * factor
-        F[0, 1] += math.sin(math.radians(theta)) * math.sin(math.radians(phi)) * factor
-        F[1, 0] += F[0, 1]
-        F[1, 1] += math.sin(math.radians(theta)) * math.cos(math.radians(phi)) * factor
+        alpha = (step + 1) / n_steps * max_stretch_factor / predicted_strain_norm
+        F[0, 0] = 1.0 + dF11 * alpha
+        F[0, 1] = dF12 * alpha
+        F[1, 0] = F[0, 1]
+        F[1, 1] = 1.0 + dF22 * alpha
 
         strain, stress = CalculateStressFromDeformationGradient(cl, properties, geometry, model_part, F)
         strain_history[step, :] = strain
@@ -204,16 +276,16 @@ while theta <= 360.0 and phi <= 360.0:
     if output_type == "plot":
         name = "neo_hookean_hyperelastic_law/strain_stress_data_case_" + str(case_number) + ".png"
         title = "Theta = " + str(theta) + " ; Phi = " + str(phi)
-        pl.plot(strain_history[:, 0], stress_history[:, 0], label="Ground truth XX", color="k")
-        pl.plot(strain_history[:, 1], stress_history[:, 1], label="Ground truth YY", color="r")
-        pl.plot(strain_history[:, 2], stress_history[:, 2], label="Ground truth XY", color="b")
+        pl.plot(strain_history[:, 0], stress_history[:, 0], label="Ground truth XX", marker='X', color="k",  markersize=5, markerfacecolor='none')
+        pl.plot(strain_history[:, 1], stress_history[:, 1], label="Ground truth YY", marker='X', color="r",  markersize=5, markerfacecolor='none')
+        pl.plot(strain_history[:, 2], stress_history[:, 2], label="Ground truth XY", marker='X', color="b",  markersize=5, markerfacecolor='none')
         pl.xlabel("Strain [-]")
         pl.ylabel("Stress [Pa]")
         pl.title(title)
         pl.legend(loc='best')
         pl.grid()
         # pl.show()
-        pl.savefig(name, dpi=300, bbox_inches='tight')
+        pl.savefig(name, dpi=600, bbox_inches='tight')
         pl.close()
 
         name = "neo_hookean_hyperelastic_law/strain_history_case_" + str(case_number) + ".png"
@@ -224,9 +296,16 @@ while theta <= 360.0 and phi <= 360.0:
         ax.set_ylabel('Strain YY')
         ax.set_zlabel('Strain XY')
         pl.title(title)
-        pl.savefig(name, dpi=300, bbox_inches='tight')
+        pl.savefig(name, dpi=600, bbox_inches='tight')
         pl.close()
         # pl.show()
+
+    with open("neo_hookean_hyperelastic_law/data_set.log", "a") as file:
+        file.write("CASE " + str(case_number) + "\n")
+        file.write("\tImposed F = " + str(F) + "\n")
+        file.write("\tImposed E = " + str(strain_history[n_steps-1, :]) + "\n")
+        file.write("\tTheta = " + str(theta) + ", Phi = " + str(phi) + "\n")
+        file.write("\tNorm of E = " + str(np.linalg.norm(strain_history[n_steps-1, :])) + "\n\n")
 
     name = "neo_hookean_hyperelastic_law/strain_stress_data_case_" + str(case_number) + ".npz"
     np.savez(name, strain_history = strain_history, stress_history = stress_history)
