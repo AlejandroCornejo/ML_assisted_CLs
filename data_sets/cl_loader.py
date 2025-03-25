1e482e4ca26bbf44281d81a7a87929e7f4d55716
RiccardoRossi <rrossi@cimne.upc.edu>
2025-03-25 15:40:31 +0100

    corrections - should now be working

--- data_sets/cl_loader.py
+++ data_sets/cl_loader.py
@@ -4,9 +4,10 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 
 class CustomDataset(Dataset):
-    def __init__(self, directory):
+    def __init__(self, directory,steps_to_consider):
         self.directory = directory
         self.files = [f for f in os.listdir(directory) if f.endswith('.npz')]
+        self.steps_to_consider = steps_to_consider
 
     def __len__(self):
         return len(self.files)
@@ -18,21 +19,21 @@ class CustomDataset(Dataset):
         all_strains = data["strain_history"]
         all_stresses = data["stress_history"]
 
-        strain_history = all_strains[1:,:]
-        stress_history = all_stresses[1:,:]
-        strain_rate = all_strains[1:,:] - all_strains[0:-1,:]
+        strain_history = all_strains[1:self.steps_to_consider,:]
+        stress_history = all_stresses[1:self.steps_to_consider,:]
+        strain_rate = all_strains[1:self.steps_to_consider,:] - all_strains[0:self.steps_to_consider-1,:]
         #print(strain)
         aux = np.sum(stress_history*strain_rate,axis=1)
 
         strain_history = torch.tensor(strain_history, dtype=torch.float32)
-        stress_history = torch.tensor(strain_history, dtype=torch.float32)
+        stress_history = torch.tensor(stress_history, dtype=torch.float32)
         work = torch.tensor(np.cumsum(aux), dtype=torch.float32)
 
         return strain_history, stress_history,work
 
 
-def get_dataloader(directory, batch_size=32, shuffle=True, num_workers=0):
-    dataset = CustomDataset(directory)
+def get_dataloader(directory, batch_size=32, shuffle=True, num_workers=0,steps_to_consider=-1):
+    dataset = CustomDataset(directory,steps_to_consider)
     print("number of files", len(dataset))
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
     return dataloader
--- data_sets/test.py
+++ data_sets/test.py
@@ -3,10 +3,12 @@ import cl_loader
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import numpy as np
 
 
 directory = ".//neo_hookean_hyperelastic_law//raw_data//"
-dataloader = cl_loader.get_dataloader(directory,batch_size=15)
+steps_to_consider = 3
+dataloader = cl_loader.get_dataloader(directory,batch_size=32,steps_to_consider=steps_to_consider)
 
 for strain_history, stress_history,work in dataloader:
     # Your training or processing code here
@@ -42,7 +44,11 @@ _ = torch.manual_seed (2023)
 class SymmetricLayer(nn.Module):
     def __init__(self, scale_estimate=1e6):
         super(SymmetricLayer, self).__init__()
-        self.C = nn.Parameter(torch.abs(torch.randn(3,3)*scale_estimate))  # Random initialization
+        # self.C = nn.Parameter(torch.abs(torch.randn(3,3)*scale_estimate))  # Random initialization
+
+        self.C = nn.Parameter(torch.tensor([[ 7.16042643e+06,  4.27997603e+02, -2.18278728e-11],
+                                    [ 4.27997603e+02,  7.14548324e+06,  7.09405867e-11],
+                                    [-2.18278728e-11,  7.09405867e-11,  3.57285779e+06]], dtype=torch.float32))
 
     def forward(self, x):
         return 0.5*torch.matmul(x,self.C+self.C.T)
@@ -57,25 +63,29 @@ class SimpleNet(nn.Module):
 
 model = SimpleNet()
 
-optimizer = optim.Adam(model.parameters(), lr=1e3) #lr=1e-3)
+optimizer = optim.Adam(model.parameters(), lr=1.0e-3) #lr=1e-3)
 def train(model, dataloader, optimizer, epochs=10):
     model.train()
 
     for epoch in range(epochs):
         epoch_loss = 0.0
         for strain_history, stress_history, target_work in dataloader:
-            # Concatenate strain_history and stress_history if needed
-            #inputs = torch.cat((strain_history, stress_history), dim=1)
-            inputs = strain_history
-
+            #TODO: this should be a tensor of order 3 not 2 totake into account batches
             strain_rate = strain_history.detach().clone()
-            strain_rate[1:,:] = strain_rate[1:,:] - strain_rate[0:-1,:]
+            strain_rate[:,1:,:] = strain_history[:,1:,:] - strain_history[:,0:-1,:]
 
             # Forward pass
-            predicted_stress_history = model(inputs)
+            predicted_stress_history = model(strain_history)
+            # print("strain",strain_history)
+            # print("stress",predicted_stress_history)
+            # print("target stress",stress_history)
+            # err
 
             err_stress = stress_history - predicted_stress_history
-            err_work = torch.sum(err_stress*strain_rate,axis=2)
+            err_work_aux = torch.sum(err_stress*strain_rate,axis=2)
+            #err_work[:,:] = torch.tensor(np.cumsum(err_work_aux[:,:]), dtype=torch.float32)
+            err_work = err_work_aux
+            #TODO: missing the scansum
             loss = torch.norm(err_work)**2
             #loss = torch.norm(stress_history - predicted_stress_history)
 
@@ -87,7 +97,8 @@ def train(model, dataloader, optimizer, epochs=10):
             epoch_loss += loss.item()
 
         print(f"Epoch [{epoch+1}/{epochs}], Loss: {epoch_loss/len(dataloader):.4f}")
-        print(model.symmetric_layer.C)
+        if epoch%10:
+            print(0.5*(model.symmetric_layer.C + model.symmetric_layer.C.T))
 
 # Example usage
 train(model, dataloader, optimizer, epochs=200)
