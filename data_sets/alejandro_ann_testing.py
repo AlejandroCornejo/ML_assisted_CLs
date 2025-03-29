32e4137ab9a260eba71df16922eaf8620348d93b
Alejandro <alejandro.cornejo23@gmail.com>
2025-03-29 18:41:51 +0100

    add colors

--- data_sets/alejandro_ann_testing.py
+++ data_sets/alejandro_ann_testing.py
@@ -42,7 +42,7 @@ model = StressPredictor()
 optimizer = optim.Adam(model.parameters(), lr = 10.0)
 
 # Training loop
-n_epochs = 10000
+n_epochs = 2000
 for epoch in range(n_epochs):
     optimizer.zero_grad()
     predicted_stress = model(ref_strain_database)
@@ -50,8 +50,8 @@ for epoch in range(n_epochs):
     predicted_work = torch.sum((ref_strain_database[:, 1 :, :] - ref_strain_database[:, : -1, :]) * predicted_stress[:, 1 :, :], axis=2) # batch x steps-2
     predicted_work = torch.cumsum(predicted_work, dim=1) # sumation along rows, horizontally
 
-    # loss = torch.mean((predicted_work - ref_work_database[:, 1:, 0]) ** 2)  # Squared difference of work
-    loss = torch.mean((predicted_stress - ref_stress_database) ** 2)  # Squared difference of work
+    loss = torch.mean((predicted_work - ref_work_database[:, 1:, 0]) ** 2)  # Squared difference of work
+    # loss = torch.mean((predicted_stress - ref_stress_database) ** 2)  # Squared difference of work
     loss.backward()
     optimizer.step()
     
@@ -59,21 +59,26 @@ for epoch in range(n_epochs):
         print(f"Epoch {epoch}, Loss: {loss.item():.6f}")
 
 
-
-
 # ===============================================================
 # Let's print the results of the ANN for one batch
-batch = [1, 25]
-strain_component = 0
-stress_component = 0
-
+batch = [1, 2, 3, 4, 6, 16]
+def GetColor(component):
+    if component == 0:
+        return "r"
+    elif component==1:
+        return "b"
+    else:
+        return "k"
 
 prediction_ANN = model(ref_strain_database)
 
 for elem in batch:
-    strain_for_print = ref_strain_database[elem, :, strain_component]
-    plt.plot(strain_for_print, ref_stress_database[elem, :, stress_component], label='REF', color='b', marker='o')
-    predicted_stress_ANN = prediction_ANN[elem, :, strain_component].detach().numpy()
-    plt.plot(strain_for_print, predicted_stress_ANN, label='ANN', color='r', marker='o')
-    plt.legend()
+    for compo in [0, 1, 2]:
+        strain_for_print = ref_strain_database[elem, :, compo]
+        plt.plot(strain_for_print, ref_stress_database[elem, :, compo], label='REF_' + str(compo), color=GetColor(compo), marker='o')
+        predicted_stress_ANN = prediction_ANN[elem, :, compo].detach().numpy()
+        plt.plot(strain_for_print, predicted_stress_ANN, label='ANN_' + str(compo), color=GetColor(compo), marker='x')
+        plt.title("batch: " + str(elem))
+        plt.legend()
     plt.show()
+
