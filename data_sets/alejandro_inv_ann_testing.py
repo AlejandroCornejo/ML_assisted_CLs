ba57c17fe15b48ed990837a6c198aa0bce469025
AlejandroCornejo <alejandro.cornejo23@gmail.com>
2025-04-17 11:57:44 +0200

    .

--- data_sets/alejandro_inv_ann_testing.py
+++ data_sets/alejandro_inv_ann_testing.py
@@ -10,8 +10,6 @@ import sklearn as sk
 # custom imports
 import cl_loader as cl_loader
 
-
-
 number_of_steps = 25
 database = cl_loader.CustomDataset("neo_hookean_hyperelastic_law/raw_data", number_of_steps)
 
@@ -24,14 +22,6 @@ print("Number of batches: ", ref_strain_database.shape[0])
 print("Number of steps  : ", ref_strain_database.shape[1])
 print("Strain size      : ", ref_strain_database.shape[2])
 
-# Why not normalizing?
-"""
-scaler = MinMaxScaler()
-normalized_db = scaler.fit_transform(database_np)
---> only works with 2Dim tensors....
-original_db = scaler.inverse_transform(normalized_db.numpy())
-"""
-
 # ==========================================================================================
 # Define the neural network
 class StressPredictor(nn.Module):
@@ -41,15 +31,8 @@ class StressPredictor(nn.Module):
     def __init__(self):
         super(StressPredictor, self).__init__()
 
-        # hidden_neurons = 1
-        # input_size = 1
-        # output_size = 1
-
         self.C1 = nn.Parameter(torch.tensor(1.0e6))
         self.C2 = nn.Parameter(torch.tensor(1.0e6))
-
-        # self.hidden = nn.Linear(input_size,     hidden_neurons,  bias=False)  # Input: input_size      -> Hidden: hidden_neurons
-        # self.output = nn.Linear(hidden_neurons, output_size,     bias=False)  # Hidden: hidden_neurons -> Output: output_size
 
     def forward(self, strain):
         batches     = strain.shape[0]
@@ -73,7 +56,7 @@ class StressPredictor(nn.Module):
 
         J = torch.linalg.det(C)**0.5 # does the det of the last two dimensions
 
-        W = nn.functional.softplus(self.C1) * (I1 - 2.0) - nn.functional.softplus(self.C1) * torch.log(J) + \
+        W = nn.functional.softplus(self.C1) * (I1 - 3.0) - nn.functional.softplus(self.C1) * torch.log(J) + \
             0.5 * nn.functional.softplus(self.C2) * (J - 1.0)**2.0
 
         grad = torch.autograd.grad(
@@ -88,10 +71,10 @@ class StressPredictor(nn.Module):
 
 # Initialize model, optimizer, and loss function
 model = StressPredictor()
-optimizer = optim.Adam(model.parameters(), lr = 1.0e4)
+optimizer = optim.Adam(model.parameters(), lr = 1000)
 
 # Training loop
-n_epochs = 3000
+n_epochs = 15000
 for epoch in range(n_epochs):
     optimizer.zero_grad()
     predicted_stress = model(ref_strain_database)
@@ -99,8 +82,7 @@ for epoch in range(n_epochs):
     predicted_work = torch.sum((ref_strain_database[:, 1 :, :] - ref_strain_database[:, : -1, :]) * predicted_stress[:, 1 :, :], axis=2) # batch x steps-2
     predicted_work = torch.cumsum(predicted_work, dim=1) # sumation along rows, horizontally
 
-    loss = torch.mean((predicted_work - ref_work_database[:, 1:, 0]) ** 2)  # Squared difference of work
-    # loss = torch.mean((predicted_work - ref_work_database[:, 1:, 0]) ** 2) + 1e2 * torch.mean((predicted_stress[:, :, :] - ref_stress_database[:, :, :]) ** 2)
+    loss = 0.5*torch.mean((predicted_work - ref_work_database[:, 1 :, 0]) ** 2)  # Squared difference of work
     # loss = torch.mean((predicted_stress - ref_stress_database) ** 2)  # Squared difference of work
     loss.backward()
     optimizer.step()
@@ -110,6 +92,20 @@ for epoch in range(n_epochs):
 
 # ===============================================================
 # Let's print the results of the ANN for one batch
+
+print("Training finished.")
+print("model parameters:")
+for name, param in model.named_parameters():
+    print(name, param.data)
+
+"""
+Training finished.
+model parameters:
+C1 tensor(3587378.7500)
+C2 tensor(-177.4099)
+
+"""
+
 batch = [1, 5, 10]
 
 def GetColor(component):
