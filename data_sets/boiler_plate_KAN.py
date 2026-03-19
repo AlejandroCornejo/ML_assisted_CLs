f8fdf48a6d28ea5670e5932b4233525e1dffa451
AlejandroCornejo <alejandro.cornejo23@gmail.com>
2026-03-19 17:28:21 +0100

    how to vectorize?

--- data_sets/boiler_plate_KAN.py
+++ data_sets/boiler_plate_KAN.py
@@ -7,8 +7,8 @@ import pykan.kan as KAN
 # -----------------------------
 # Data
 # -----------------------------
-x = np.linspace(0, 1, 100)
-y = np.exp(-0.5 * ((x - 0.5) / 0.1)**2)
+x = np.linspace(0, 1, 500)
+y = np.exp(-0.5 * ((x-0.25) / 0.1 )**2)
 
 x_torch = torch.tensor(x, dtype=torch.float32).unsqueeze(1)  # (100,1)
 y_torch = torch.tensor(y, dtype=torch.float32).unsqueeze(1)  # (100,1)
@@ -16,11 +16,14 @@ y_torch = torch.tensor(y, dtype=torch.float32).unsqueeze(1)  # (100,1)
 # -----------------------------
 # Model
 # -----------------------------
-model = KAN.MultKAN(
+model = KAN.MultKAN( # x--[]-->y
     width=[1, 1],
-    grid=7,   # slightly richer
-    k=3       # cubic splines
+    grid=6,
+    k=3,
+    grid_range=[0, 1]
 )
+model.speed()
+
 
 # -----------------------------
 # Optimizer
@@ -30,22 +33,21 @@ optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 # -----------------------------
 # Training loop (L2 minimization)
 # -----------------------------
-n_epochs = 400
+n_epochs = 4000
 
 for epoch in range(n_epochs):
     optimizer.zero_grad()
 
-    y_pred_list = []
+    # y_pred_list = []
+    # for i in range(len(x)):
+    #     x_i = x_torch[i].view(1, 1)  # already tensor, no need to recreate
+    #     y_pred_i = model(x_i)        # keep tensor with grad
+    #     y_pred_list.append(y_pred_i)
 
-    for i in range(len(x)):
-        x_i = x_torch[i].view(1, 1)  # already tensor, no need to recreate
-        y_pred_i = model(x_i)        # keep tensor with grad
-        if i > 75:
-            y_pred = 0.1
-        y_pred_list.append(y_pred_i)
+    y_pred = model(x_torch)  # shape (100,1)
 
     # Stack into a tensor
-    y_pred = torch.vstack(y_pred_list)  # shape (100,1)
+    # y_pred = torch.vstack(y_pred_list)  # shape (100,1)
 
     # L2 loss
     loss = torch.mean((y_pred - y_torch)**2)
